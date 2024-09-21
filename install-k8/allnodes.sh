ff06f321f494cb2b5f88c04811e8cb0c8df326c5
aadspinner <aadspinner@gmail.com>
2024-09-21 20:51:27 +0530

    addded insatll k8

--- install-k8/allnodes.sh
+++ install-k8/allnodes.sh
@@ -1,27 +1,18 @@
+
 sudo apt update -y
-sudo apt install -y docker.io
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sudo apt update
+sudo apt update -y
 sudo apt install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-kubeadm init --control-plane-endpoint 13.202.18.177 --pod-network-cidr 192.168.0.0/16 --apiserver-advertise-address 13.202.18.177  --ignore-preflight-errors all
-#--apiserver-advertise-address 13.202.18.177  --ignore-preflight-errors all
-# kubeadm reset remove-etcd-member cleanup-node
-# sysctl params required by setup, params persist across reboots
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.ipv4.ip_forward = 1
-EOF
 
-# Apply sysctl params without reboot
-sudo sysctl --system
+## installing containerd.io
 
-
-sudo apt-get update 
-sudo apt-get install ca-certificates curl
+sudo apt-get update -y 
+sudo apt-get -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -31,8 +22,24 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update 
+ sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+containerd config default > /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo apt-get update -y
+
+# Install jq, a command-line JSON processor
+sudo apt-get install -y jq
+
+# Retrieve the local IP address of the eth0 interface and set it for kubelet
+local_ip="$(ip --json addr show eth1 | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')"
+
+# Write the local IP address to the kubelet default configuration file
+cat > /etc/default/kubelet << EOF
+KUBELET_EXTRA_ARGS=--node-ip=$local_ip
+EOF
 sudo systemctl restart containerd
+
 
